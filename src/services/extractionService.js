b91e4d22140be2277b68ddcac8a8760fb188b3e6
Raymond Rhodes <rayrhodes@MacBook-Air-8.local>
2025-12-03 16:57:30 -0500

    Remove Puppeteer entirely - causing Render deploy to hang

--- src/services/extractionService.js
+++ src/services/extractionService.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const puppeteer = require('puppeteer');
 const config = require('../config');
 const logger = require('../utils/logger');
 const { ExtractionError, ErrorTypes, isRetryable, getBackoffDelay } = require('../utils/errors');
